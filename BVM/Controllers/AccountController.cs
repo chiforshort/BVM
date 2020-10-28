93483727374b831e76a000439a785b4da7a4cf5b
asia chicon <chiconba@my.erau.edu>
2020-10-28 15:17:50 -0400

    Addition of background images and fixing login

---
+++ BVM/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BVM.Models;
+
+namespace BVM.Controllers
+{
+    public class AccountController : Controller
+    {[HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Verify(Account acc)
+        {
+            return View();
+        }
+    }
+}
